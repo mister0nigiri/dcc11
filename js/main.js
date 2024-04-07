e847e4e0d88feb1697549aa5a6cf116160a23dfb
miyashun <gongyongjunyou@gmail.com>
2024-04-08 02:05:46 +0900

    本日の作業終了

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// document.querySelector(".header__hamburger").addEventListener("click", function() {
+//   this.classList.toggle('active');
+// });
+
+
+// $(".header__hamburger").click(function () {//ボタンがクリックされたら
+//     $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+//     $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+// });
+
+// $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+//     $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+//     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+// });
